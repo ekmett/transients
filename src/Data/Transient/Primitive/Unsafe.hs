e0ece8d0ac32305d4b4ee48f1e3568f1a1eabef3
Edward Kmett <ekmett@gmail.com>
2015-08-07 20:13:49 -0400

    more unsafe primitives for working with possibly-frozen mutable arrays

--- src/Data/Transient/Primitive/Unsafe.hs
+++ src/Data/Transient/Primitive/Unsafe.hs
@@ -2,15 +2,53 @@
 {-# LANGUAGE MagicHash #-}
 {-# LANGUAGE UnboxedTuples #-}
 {-# LANGUAGE UnliftedFFITypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE GHCForeignImportPrim #-}
 {-# LANGUAGE ForeignFunctionInterface #-}
 module Data.Transient.Primitive.Unsafe
-  ( checkMutableArray#
-  , checkSmallMutableArray#
+  ( unsafeCheckMutableArray
+  , unsafeCheckSmallMutableArray
+  , unsafeIndexMutableArray
+  , unsafeIndexMutableArrayM
+  , unsafeIndexSmallMutableArray
+  , unsafeIndexSmallMutableArrayM
+  -- * Custom foreign primitives
+  , unsafeCheckMutableArray#
+  , unsafeCheckSmallMutableArray#
   ) where
 
+import Control.Monad.Primitive
+import Data.Primitive.Array
+import Data.Transient.Primitive.SmallArray
+import GHC.Exts
 import GHC.Prim
+import Unsafe.Coerce
 
--- check if this mutable array is actually still mutable internally.
-foreign import prim "checkMutableArrayzh"      checkMutableArray#      :: MutableArray# s a -> State# s -> (# State# s, Int# #) 
-foreign import prim "checkSmallMutableArrayzh" checkSmallMutableArray# :: SmallMutableArray# s a -> State# s -> (# State# s, Int# #)
+foreign import prim "checkMutableArrayzh"      unsafeCheckMutableArray#      :: MutableArray# s a -> State# s -> (# State# s, Int# #) 
+foreign import prim "checkSmallMutableArrayzh" unsafeCheckSmallMutableArray# :: SmallMutableArray# s a -> State# s -> (# State# s, Int# #)
+
+-- | This returns 'True' if the 'MutableArray' is unfrozen and can still be mutated.
+unsafeCheckMutableArray :: PrimMonad m => MutableArray (PrimState m) a -> m Bool
+unsafeCheckMutableArray (MutableArray m) = primitive $ \s -> case unsafeCheckMutableArray# m s of
+  (# s', i #) -> (# s', isTrue# i #)
+
+-- | This returns 'True' if the 'SmallMutableArray' is unfrozen and can still be mutated.
+unsafeCheckSmallMutableArray :: PrimMonad m => SmallMutableArray (PrimState m) a -> m Bool
+unsafeCheckSmallMutableArray (SmallMutableArray m) = primitive $ \s -> case unsafeCheckSmallMutableArray# m s of
+  (# s', i #) -> (# s', isTrue# i #)
+
+unsafeIndexMutableArray :: MutableArray s a -> Int -> a
+unsafeIndexMutableArray = unsafeCoerce indexArray
+{-# INLINE unsafeIndexMutableArray #-}
+
+unsafeIndexMutableArrayM :: forall m s a. Monad m => MutableArray s a -> Int -> m a
+unsafeIndexMutableArrayM = unsafeCoerce (indexArrayM :: Array a -> Int -> m a)
+{-# INLINE unsafeIndexMutableArrayM #-}
+
+unsafeIndexSmallMutableArray :: SmallMutableArray s a -> Int -> a
+unsafeIndexSmallMutableArray = unsafeCoerce indexSmallArray
+{-# INLINE unsafeIndexSmallMutableArray #-}
+
+unsafeIndexSmallMutableArrayM :: forall m s a. Monad m => SmallMutableArray s a -> Int -> m a
+unsafeIndexSmallMutableArrayM = unsafeCoerce (indexSmallArrayM :: SmallArray a -> Int -> m a)
+{-# INLINE unsafeIndexSmallMutableArrayM #-}
